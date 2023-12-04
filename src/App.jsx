484431fbd166790a1ed920b7162f01c6f6379399
macikb8@yahoo.com <macikb8@yahoo.com>
2023-12-04 12:59:13 +1100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Button from './components/Button'
+
+function App() {
+
+  return (
+    <>
+    <Button label="hel" />
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
